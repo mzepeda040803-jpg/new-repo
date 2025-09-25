88f9e2fd60e823b465d6e115e4cf1ddcb27631c4
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-09-25 13:31:27 -0400

    commit

---
+++ src/main/java/MathApp.java
@@ -0,0 +1,9 @@
+public class MathApp
+
+{public class MathApp {
+    public static void main(String[] args) {
+        int natKidCount = 2, brittKidCount = 4;
+        int mostKids = Math.max(natKidCount, brittKidCount);
+        System.out.println("The biggest family has " + mostKids + " kids");
+    }
+}
